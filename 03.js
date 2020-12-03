ffb82197f42d42740800f389e871733c8514f181
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-03 09:44:21 -0800

    day 03 part 2

--- 03.js
+++ 03.js
@@ -1,14 +1,18 @@
 import input from "./inputs/input-03.js";
 import _ from "lodash";
 import { YEAR } from "./constants.js";
+const rows = input.split("\n");
+const height = rows.length;
+const grid = rows.map((row) => row.split(""));
+const width = grid[0].length;
 
-console.log(partOne());
-// console.log(partTwo());
+// console.log(partOne());
+console.log(partTwo());
 function partOne() {
-  const rows = input.split("\n");
-  const height = rows.length;
-  const grid = rows.map((row) => row.split(""));
-  const width = grid[0].length;
+  return toboggan(3, 1);
+}
+
+function toboggan(dx, dy) {
   let x = 0;
   let y = 0;
   let trees = 0;
@@ -16,10 +20,18 @@ function partOne() {
     if (grid[y][x % width] === "#") {
       trees++;
     }
-    x += 3;
-    y += 1;
+    x += dx;
+    y += dy;
   }
   return trees;
 }
 
-function partTwo() {}
+function partTwo() {
+  return (
+    toboggan(1, 1) *
+    toboggan(3, 1) *
+    toboggan(5, 1) *
+    toboggan(7, 1) *
+    toboggan(1, 2)
+  );
+}
