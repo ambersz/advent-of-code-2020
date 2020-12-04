e146be63f5f0ed729e2ccc0e9ff5608753fc84a6
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-03 21:51:53 -0800

    Day 04 Part 2

--- 04.js
+++ 04.js
@@ -22,7 +22,7 @@ function inputSetup() {
       return pass;
     });
 }
-
+let validList = [];
 function partOne() {
   const requiredFields = [
     "byr",
@@ -34,21 +34,51 @@ function partOne() {
     "pid",
     // "cid",
   ];
-  let count = 0;
-  processedInput.forEach(
-    (p) =>
-      requiredFields.reduce(
-        (valid, fieldName) => valid && p[fieldName],
-        true
-      ) && count++
+  validList = processedInput.filter((p) =>
+    requiredFields.reduce((valid, fieldName) => valid && p[fieldName], true)
   );
-  return count;
+  return validList.length;
 }
 
 function partTwo() {
-  processedInput;
+  return validList
+    .filter((p) => {
+      if (p.byr.length !== 4) return false;
+      const yr = parseInt(p.byr);
+      return yr >= 1920 && yr <= 2002;
+    })
+    .filter((p) => {
+      if (p.iyr.length !== 4) return false;
+      const yr = parseInt(p.iyr);
+      return yr >= 2010 && yr <= 2020;
+    })
+    .filter((p) => {
+      if (p.eyr.length !== 4) return false;
+      const yr = parseInt(p.eyr);
+      console.log(yr);
+      return yr >= 2020 && yr <= 2030;
+    })
+    .filter((p) => {
+      if (p.hgt.indexOf("cm") !== -1) {
+        const ht = parseInt(p.hgt);
+        return ht >= 150 && ht <= 193;
+      } else if (p.hgt.indexOf("in") !== -1) {
+        const ht = parseInt(p.hgt);
+        return ht >= 59 && ht <= 76;
+      }
+      return false;
+    })
+    .filter((p) => {
+      return p.hcl?.match(/^#[0-9a-f]{6}$/)?.length === 1;
+    })
+    .filter((p) => {
+      return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(p.ecl);
+    })
+    .filter((p) => {
+      return p.pid.match(/^[0-9]{9}$/)?.length === 1;
+    }).length;
 }
 
 const processedInput = inputSetup();
 console.log(partOne());
-// console.log(partTwo());
+console.log(partTwo());
