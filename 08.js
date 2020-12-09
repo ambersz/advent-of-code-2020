6c7e5c905d3fa8e166de72be4e8872cd2c338a9e
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-08 16:42:57 -0800

    Day 08 Part 2

--- 08.js
+++ 08.js
@@ -13,31 +13,42 @@ function inputSetup() {
   });
 }
 
-function partOne() {
+function partOne(input, reverseIndex) {
+  const processedInput = input.map((a)  =>  ({  ...a  }));
   let i = 0;
   let acc = 0;
+
   while (!processedInput[i].run) {
     // console.log(i);
     const op = processedInput[i];
     op.run = true;
-    if (op.op === "nop") {
+    if (op.op === "nop"|(op.op === "jmp" && i===reverseIndex)) {
       i++;
     } else if (op.op === "acc") {
       acc += op.val;
       i++;
-    } else if (op.op === "jmp") {
+    } else if (op.op === "jmp"|(op.op === "nop" && i===reverseIndex)) {
       i += op.val;
     } else {
       console.error("!!!!");
+    }
+    if (i >= processedInput.length) {
+      console.log("terminated");
+      return {acc, terminated:true};
     }
   }
   return acc;
 }
 
 function partTwo() {
-  processedInput;
+  let i = 0;
+  while (true) {
+    const res = partOne(processedInput, i);
+    if (res.terminated) return res.acc;
+    i++
+  }
 }
 
 const processedInput = inputSetup();
-console.log(partOne());
-// console.log(partTwo());
+// console.log(partOne(processedInput));
+console.log(partTwo());
