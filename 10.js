6e4c5850acacfa9b51ca8d96c3fdd723238e9d82
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-09 21:32:05 -0800

    Day 10 Part 2

--- 10.js
+++ 10.js
@@ -1,7 +1,7 @@
 import input from "./inputs/input-10.js";
 import _ from "lodash";
 import { YEAR } from "./constants.js";
-
+let diffs;
 function inputSetup() {
   input.push(0);
   const partial = _.sortBy(input);
@@ -10,9 +10,7 @@ function inputSetup() {
 }
 
 function partOne() {
-  const diffs = processedInput
-    .map((n, i) => processedInput[i + 1] - n)
-    .slice(0, -1);
+  diffs = processedInput.map((n, i) => processedInput[i + 1] - n).slice(0, -1);
   console.log(processedInput);
   console.log(diffs);
   return (
@@ -21,9 +19,29 @@ function partOne() {
 }
 
 function partTwo() {
-  console.log(processedInput);
+  const wayMap = {
+    0: 1Zz,
+    1: 1,
+    2: 2,
+    3: 4,
+    4: 7,
+  };
+  let one = 0;
+  let ways = 1;
+  diffs.forEach((n) => {
+    if (n === 1) {
+      one++;
+    } else if (n === 3) {
+      // console.log(one);
+      ways *= wayMap[one];
+      one = 0;
+    } else {
+      console.log("!!!");
+    }
+  });
+  return ways;
 }
 
 const processedInput = inputSetup();
 console.log(partOne());
-// console.log(partTwo());
+console.log(partTwo());
